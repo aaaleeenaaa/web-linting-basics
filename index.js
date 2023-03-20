b865820c76c490910de5f13dc95639e2a471cc71
Alena Pferdekemper <alena.pferdekemper@web.de>
2023-03-20 16:42:05 +0100

    bug fixing

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
+  const cards = document.querySelectorAll('[data-js=card]');
+  cards.forEach(() => {
+    const button = document.querySelector('[data-js="button"]');
+    const link = document.querySelector('[data-js="link"]');
+    const arrow = document.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hide');
+      arrow.classList.toggle('rotate');
+    });
+  });
 }
+
+toggleButton();
